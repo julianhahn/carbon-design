ddb5e34c00b505a64ca0514c3b2c9cc46734657b
julian hahn <julianhahn01@gmail.com>
2022-07-09 00:41:45 +0200

    inital commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Button } from '@carbon/react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
+        tutorial.
+      </div>
+      <Button>Button</Button>
+
+    </div>
+  );
+}
+
+export default App;
